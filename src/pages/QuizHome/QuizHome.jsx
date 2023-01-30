2170fbb621ba59133ec145a2e8856a976fc4a663
jfhaines <joefhaines@gmail.com>
2023-01-31 02:49:05 +1000

    Added Quiz Home page

--- src/pages/QuizHome/QuizHome.jsx
+++ src/pages/QuizHome/QuizHome.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import Title from '@/components/Title/Title.jsx'
+import Button from '@/components/Button/Button.jsx'
+import cardDeck from '../../assets/card-deck.svg'
+import './style/QuizHome.css'
 
-const QuizHome = () => {
+const QuizHome = ({ quiz }) => {
+    const generateCardDeck = () => {
+        let cardArray = []
+        for (let i = 0; i < 5; i++) {
+            cardArray.push(<div className='card-in-deck'></div>)
+        }
+        cardArray.push(<div className='card-in-deck top-card-in-deck'><p>{quiz.flashcards ? quiz.flashcards[0].question : ''}</p></div>)
+        return cardArray
+    }
+
     return (
-        <div>QuizHome</div>
+        <main className='main-quiz-home'>
+            <div className='quiz-home-top'>
+                <Title subheading='Home' heading={quiz.name ? quiz.name.toUpperCase() : ''} />
+                <Button type='secondary'>Delete</Button>
+            </div>
+            <div className={'card-deck'}>
+                {generateCardDeck()}
+            </div>
+            <div className='quiz-home-bottom'>
+                <Button type='primary'>Take Quiz</Button>
+                <Button type='secondary'>Update</Button>
+            </div>
+        </main>
     )
 }
 
--- src/routes/Quiz.jsx
+++ src/routes/Quiz.jsx
@@ -11,6 +11,52 @@ const Quiz = () => {
     useEffect(() => {
         // fetchQuiz()
         // setQuiz(quiz)
+        setQuiz({
+            _id: 389327438328,
+            name: 'Quiz One',
+            flashcards: [
+                { 
+                    _id: 40943805890843,
+                    question: "What does 'es tut meir leid' mean in German?",
+                    answers: [
+                        { text: 'Excuse me', isCorrectOption: false },
+                        { text: "I'm sorry", isCorrectOption: true },
+                        { text: 'Goodbye', isCorrectOption: false },
+                        { text: 'Hello', isCorrectOption: false },
+                    ]
+                },
+                { 
+                    _id: 40943805890843,
+                    question: "What does 'es tut meir leid' mean in German?",
+                    answers: [
+                        { text: 'Excuse me', isCorrectOption: false },
+                        { text: "I'm sorry", isCorrectOption: true },
+                        { text: 'Goodbye', isCorrectOption: false },
+                        { text: 'Hello', isCorrectOption: false },
+                    ]
+                },
+                { 
+                    _id: 40943805890843,
+                    question: "What does 'es tut meir leid' mean in German?",
+                    answers: [
+                        { text: 'Excuse me', isCorrectOption: false },
+                        { text: "I'm sorry", isCorrectOption: true },
+                        { text: 'Goodbye', isCorrectOption: false },
+                        { text: 'Hello', isCorrectOption: false },
+                    ]
+                },
+                { 
+                    _id: 40943805890843,
+                    question: "What does 'es tut meir leid' mean in German?",
+                    answers: [
+                        { text: 'Excuse me', isCorrectOption: false },
+                        { text: "I'm sorry", isCorrectOption: true },
+                        { text: 'Goodbye', isCorrectOption: false },
+                        { text: 'Hello', isCorrectOption: false },
+                    ]
+                }
+            ]
+        })
     })
 
     return (
