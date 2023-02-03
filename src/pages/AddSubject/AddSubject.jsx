3383ac2bd3845e977df885fc4957f5206ba43dc9
jfhaines <joefhaines@gmail.com>
2023-02-03 19:31:00 +1000

    Added add subject page

--- src/pages/AddSubject/AddSubject.jsx
+++ src/pages/AddSubject/AddSubject.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Card from '@/components/Card/Card.jsx'
+import Title from '@/components/Title/Title.jsx'
+import TextArea from '@/components/TextArea/TextArea.jsx'
+import Button from '@/components/Button/Button.jsx'
+import './style/AddSubject.css'
 
 const AddSubject = () => {
+    const [ subjectName, setSubjectName ] = useState('')
+
     return (
-        <div>AddSubject</div>
+        <main className='add-subject'>
+            <div className='outer-content-wrapper'>
+                <Title subheading='Add New' heading='SUBJECT' />
+                <Card className='add-subject-form'>
+                    <h3 className='subject-name'>Subject Name</h3>
+                    <TextArea />
+                    <Button type='1' size='1'>Create</Button>
+                </Card>
+            </div>
+        </main>
     )
 }
 
