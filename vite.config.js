3fbfd2ef4e71f1e0f129b79883f1e1e1f2e1f901
2w00fs <63689305+2w00fs@users.noreply.github.com>
2023-02-07 10:54:29 +1000

    added process.env to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,41 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
+export default ({ mode }) => {
+    // Load app-level env vars to node-level env vars.
+    process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+    
+    return defineConfig({
+        server: { port:  process.env.PORT },
+        plugins: [react()],
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, './src'),
+            },
+        },
+        test: {
+            environment: 'jsdom',
+            globals: true
+        }
+    })
+};
+
+
+
+
+
+/*
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import dotenv from 'dotenv'
+
+import PORT from process().env.PORT
+
 // https://vitejs.dev/config/
-export default defineConfig({ 
+export default defineConfig({
+    server: {port:8080},
     plugins: [react()],
     resolve: {
         alias: {
@@ -15,3 +47,4 @@ export default defineConfig({
         globals: true
     }
 })
+*/
